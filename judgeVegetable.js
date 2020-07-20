6c88fafb18dac3a776f22e686796341c374763ec
Nicole Woodcock <nicole.xox@gmail.com>
2020-07-20 05:05:12 +0000

    Completed 'We're rooting for you\!' assignment

---
+++ judgeVegetable.js
@@ -0,0 +1,33 @@
+function judgeVegetable(vegetables, metric) {
+  let winner = vegetables[0]
+  for (let v = 1; v < vegetables.length; v++) {
+    const currentVeg = vegetables[i]
+    if (winner[metric] < currentVeg[metric]) {
+      winner = currentVeg
+    }
+  }
+  return winner.submitter
+}
+
+
+
+const vegetables = [{
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+console.log(judgeVegetable(vegetables, metric))
